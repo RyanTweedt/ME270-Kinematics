294e8e90246e1709aa130cf7de6308b0e77901e7
RyanTweedt <rtweedt@iastate.edu>
2013-06-08 22:07:25 -0500

    Working animation except for a discontinuity issue for the link positioning equations

--- animatedModel.lua
+++ animatedModel.lua
@@ -75,14 +75,28 @@ Actions.addFrameAction(function()		--reposition moving pieces accordingly
 		B = 2*r1*r4*math.sin(theta1) - 2*r2*r4*math.sin(THETA2)	
 		C = r1^2 + r2^2 + r4^2 - r3^2 - 2*r1*r2*(math.cos(theta1)*math.cos(THETA2) + math.sin(theta1)*math.sin(THETA2))
 		t = (-B + math.sqrt(B^2 - C^2 + A^2))/(C - A)
+		
 		theta4 = 2*math.atan(t)
 		theta3 = math.atan((r1*math.sin(theta1) + r4*math.sin(theta4) - r2*math.sin(THETA2))/(r1*math.cos(theta1) + r4*math.cos(theta4) - r2*math.cos(THETA2)))
+		
 		shift2x = (p1.x - r2*math.cos(THETA2) - p2.x)*scale
 		shift2y = (p1.y + r2*math.sin(THETA2) - p2.y)*scale
-		shift3x = (p2.x - r3*math.cos(theta3) - p3.x)*scale
-		shift3y = (p2.y + r3*math.sin(theta3) - p3.y)*scale
+		
+		shift3x = (r4*math.cos(3.14159265358979 - theta4) - r4)*scale
+		shift3y = (r4*math.sin(3.14159265358979 - theta4) - 0.75)*scale
 		dT2 = -(THETA2 - 6.085789747)
 
+		shiftLinkx = (math.sqrt((p2.x*scale)^2+(p2.y*scale)^2))*math.cos(math.atan((p2.y*scale)/(p2.x*scale))) - (math.sqrt((p2.x*scale)^2+(p2.y*scale)^2))*math.cos((-(theta3 + 1.5708)+math.atan((p2.y*scale)/(p2.x*scale))))
+		shiftLinky = (math.sqrt((p2.x*scale)^2+(p2.y*scale)^2))*math.sin(math.atan((p2.y*scale)/(p2.x*scale))) - (math.sqrt((p2.x*scale)^2+(p2.y*scale)^2))*math.sin((-(theta3 + 1.5708)+math.atan((p2.y*scale)/(p2.x*scale))))
+		
+		
+		--animate
+		if THETA2 > 7.2 then
+			THETA2 = 5.9
+		else
+			THETA2 = THETA2 + 0.0075
+		end
+		Actions.waitSeconds(0.001)
 		Actions.waitForRedraw()
 	end
 end)
@@ -94,23 +108,29 @@ end)
 Actions.addFrameAction(function()
 		while true do
 			
-			xform[4]:setPosition(osg.Vec3d(shift2x, shift2y, 0))
+			xform[4]:setPosition(osg.Vec3d(shift2x, shift2y, 0))  --top bolt good
 			
-			xform[5]:setPosition(osg.Vec3d(shift3x, shift3y, 0))
+			xform[5]:setPosition(osg.Vec3d(shift3x, shift3y, 0))  --bot bolt good
 			
+			xform[8]:setAttitude(osg.Quat(-(theta3 + 1.5708),osg.Vec3d(0,0,1)))
 			xform[9]:setAttitude(osg.Quat(-(theta3 + 1.5708),osg.Vec3d(0,0,1)))
+
+			xform[8]:setPosition(osg.Vec3d(shift2x + shiftLinkx, shift2y + shiftLinky, 0))
+			xform[9]:setPosition(osg.Vec3d(shift2x + shiftLinkx, shift2y + shiftLinky, 0))
 			
-			xform[8]:setPosition(osg.Vec3d(((r2 - r2*math.cos(THETA2)))*scale,(r2*math.sin(THETA2)-r2*math.sin(6.085789747))*scale,0))
-			xform[9]:setPosition(osg.Vec3d(((r2 - r2*math.cos(THETA2)) - r2*math.cos(math.atan(shift2y/shift2x) + (theta3 + 1.5708)) - shift2x)*scale,(r2*math.sin(THETA2)-r2*math.sin(6.085789747) + shift2y - r2*math.sin(math.atan(shift2y/shift2x) + (theta3 + 1.5708)))*scale,0))
 			
-			xform[10]:setAttitude(osg.Quat(dT2,osg.Vec3d(0,0,1)))
+			xform[10]:setAttitude(osg.Quat(dT2,osg.Vec3d(0,0,1)))  --lever good
 			xform[10]:setPosition(osg.Vec3d((p1.x - (math.sqrt(p1.x^2 + p1.y^2)*math.cos(dT2 + math.atan(p1.y/p1.x))))*scale, -((math.sqrt(p1.x^2 + p1.y^2)*math.sin(dT2 + math.atan(p1.y/p1.x))) - p1.y)*scale, 0))
 
+			xform[11]:setAttitude(osg.Quat(3.14159265358979 - theta4,osg.Vec3d(0,0,1)))  --pusher
+			xform[11]:setPosition(osg.Vec3d(     (p4.x - (math.sqrt(p4.x^2 + p4.y^2))*math.cos((3.14159265358979 - theta4) + (math.atan(p4.y/p4.x))))*scale, ((math.sqrt(p4.x^2 + p4.y^2))*math.sin((3.14159265358979 - theta4) + (math.atan(p4.y/p4.x))) - p4.y)*scale,0))
+			
 			Actions.waitForRedraw()
 		end
 	end)
 	
 --------------------------------------------------------------------------------------------------------------------------------------------	
+
 	
 --18 = the plunger
 
