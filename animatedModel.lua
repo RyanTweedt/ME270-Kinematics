074f1d8e26a847145a719040f94cd4876e74e036
RyanTweedt <rtweedt@iastate.edu>
2013-06-13 16:50:31 -0500

    Working program V1.0
    Changes:
    -Added the basic factory model for the enviroment
    -Added Navigation.lua into the script
    -Fixed the issues with the 4-bar linkage movement
    -Added control of the animation with the wand

--- animatedModel.lua
+++ animatedModel.lua
@@ -4,73 +4,71 @@ require ("TransparentGroup")
 
 vrjLua.appendToModelSearchPath(getScriptFilename())
 
---dofile(vrjLua.findInModelSearchPath([[DrawableShapesMod.lua]]))
-dofile(vrjLua.findInModelSearchPath([[simpleLights.lua]]))
+dofile(vrjLua.findInModelSearchPath([[libraries\simpleLights.lua]]))
+dofile(vrjLua.findInModelSearchPath([[libraries\useful_tools.lua]]))
+dofile(vrjLua.findInModelSearchPath([[libraries\loadBasicFactory.lua]]))
+dofile(vrjLua.findInModelSearchPath([[libraries\Navigation.lua]]))
 
---[[
-require neccessary files
-load in model parts
-
-attach vectors to parts
-add frameactions to animate the parts
-]]
 
 --initial values for positions of moving parts
-theta1 = 5.977306			--constant angle of the closed vector system
-THETA2 = 6.085789747		--driven angle for the closed vector system
-theta3 = -1.5708
-theta4 = 3.092452
-dT2 = 0 --The angle THETA2 has changed
+local theta1 = 5.977306			--constant angle of the closed vector system
+local THETA2 = 6.085789747		--driven angle for the closed vector system
+local theta3 = -1.5708
+local theta4 = 3.092452
 
-r1 = 19.92486				--the length of vector r1, the distance from the lever pivot to the pusher pivot
-r2 = 3.824265				--the length of vector r2, the distance to the top bolt from the lever pivot
-r3 = 4.5					--the length of the link pices
-r4 = 15.26843				--the length the pusher
+local r1 = 19.92486				--the length of vector r1, the distance from the lever pivot to the pusher pivot
+local r2 = 3.824265				--the length of vector r2, the distance to the top pin from the lever pivot
+local r3 = 4.5					--the length of the link pices
+local r4 = 15.26843				--the length the pusher
 
-offsetx = 0
+offsetx = 0.11
 offsety = 0
 
-p1 = {x = 20.75 + offsetx, y = 19.25 + offsety, z = 0}				--location of the lever pivot, 	static
-p2 = {x = 17 + offsetx, y = 18.5 + offsety, z = 0}					--location of the top bolt, 	dynamic
-p3 = {x = 17 + offsetx, y = 14 + offsety, z = 0}					--location of the bottom bolt,	dynamic
-p4 = {x = 1.75 + offsetx, y = 13.25 + offsety, z = 0}				--location of the pusher pivot,	static
+local p1 = {x = 20.75 + offsetx, y = 19.25 + offsety, z = 0}				--location of the lever pivot, 	static
+local p2 = {x = 17 + offsetx, y = 18.5 + offsety, z = 0}					--location of the top pin, 	dynamic
+local p3 = {x = 17 + offsetx, y = 14 + offsety, z = 0}					--location of the bottom pin,	dynamic
+local p4 = {x = 1.75 + offsetx, y = 13.25 + offsety, z = 0}				--location of the pusher pivot,	static
 
-scale = ScaleFrom.inches
-
+local scale = ScaleFrom.inches
 
 --dumy variables used to compute angles and positions
-A = 465.6871601
-B = -160.3185305
-C = 473
-t = 40.69124199
+local A = 465.6871601
+local B = -160.3185305
+local C = 473
+local t = 40.69124199
 
-part = {}					--create an arrays to store all the parts
-xform = {}					--create an array to store the transforms for the parts that will be moving
+local part = {}					--create an arrays to store all the parts
+local xform = {}					--create an array to store the transforms for the parts that will be moving
 
-for i =1,31 do				--add all the parts to the scene filtering out all of the parts that will be moving and adding them to transforms
-	if i == 4 or 5 or 8 or 9 or 10 or 11 then
-		xform[i] = osg.PositionAttitudeTransform()
-		RelativeTo.World:addChild(xform[i])
-		xform[i]:addChild(Model("OSG files/Assembly ("..i..").STL.osg")) 
-		xform[i]:addChild(Sphere{ radius = .02, position = {0, 0, 0}})
-		
-	else
-		part[i] = Model("OSG files/Assembly ("..i..").STL.osg")
-		RelativeTo.World:addChild(part[i])
-	end
-end
-
---[[
-xform[4]	top bolt (p2)
-xform[5]	bottom bolt (p3)
-xform[8]	link
-xform[9]	link
-xform[10]	lever
-xform[11]	pusher
+--[[Legend:
+		xform[4]	top pin
+		xform[5]	bottom pin
+		xform[8]	link
+		xform[9]	link
+		xform[10]	lever
+		xform[11]	pusher
 ]]
 
+local device = gadget.PositionInterface("VJWand")
+
+local repos = Transform{
+	position = {1.5,0.5,0},
+	}
+for i =1,31 do				--add all the parts to the scene
+	xform[i] = osg.PositionAttitudeTransform()
+	xform[i]:addChild(Model("models/OSG files/Assembly ("..i..").STL.osg")) 
+	xform[i]:addChild(Sphere{ radius = .02, position = {0, 0, 0}})
+	changeTransformColor(xform[i], getRandomColor())
+	repos:addChild(xform[i])
+end
+
+RelativeTo.World:addChild(repos)
+
 Actions.addFrameAction(function()		--reposition moving pieces accordingly
+	local upButton = gadget.DigitalInterface("VJButton1")
+	local downButton = gadget.DigitalInterface("VJButton2")
 	while true do
+	
 		A = 2*r1*r4*math.cos(theta1) - 2*r2*r4*math.cos(THETA2)
 		B = 2*r1*r4*math.sin(theta1) - 2*r2*r4*math.sin(THETA2)	
 		C = r1^2 + r2^2 + r4^2 - r3^2 - 2*r1*r2*(math.cos(theta1)*math.cos(THETA2) + math.sin(theta1)*math.sin(THETA2))
@@ -79,59 +77,70 @@ Actions.addFrameAction(function()		--reposition moving pieces accordingly
 		theta4 = 2*math.atan(t)
 		theta3 = math.atan((r1*math.sin(theta1) + r4*math.sin(theta4) - r2*math.sin(THETA2))/(r1*math.cos(theta1) + r4*math.cos(theta4) - r2*math.cos(THETA2)))
 		
-		shift2x = (p1.x - r2*math.cos(THETA2) - p2.x)*scale
-		shift2y = (p1.y + r2*math.sin(THETA2) - p2.y)*scale
+		local shiftTopPinx = (p1.x - r2*math.cos(THETA2) - p2.x)*scale
+		local shiftTopPiny = (p1.y + r2*math.sin(THETA2) - p2.y)*scale
 		
-		shift3x = (r4*math.cos(3.14159265358979 - theta4) - r4)*scale
-		shift3y = (r4*math.sin(3.14159265358979 - theta4) - 0.75)*scale
-		dT2 = -(THETA2 - 6.085789747)
-
-		shiftLinkx = (math.sqrt((p2.x*scale)^2+(p2.y*scale)^2))*math.cos(math.atan((p2.y*scale)/(p2.x*scale))) - (math.sqrt((p2.x*scale)^2+(p2.y*scale)^2))*math.cos((-(theta3 + 1.5708)+math.atan((p2.y*scale)/(p2.x*scale))))
-		shiftLinky = (math.sqrt((p2.x*scale)^2+(p2.y*scale)^2))*math.sin(math.atan((p2.y*scale)/(p2.x*scale))) - (math.sqrt((p2.x*scale)^2+(p2.y*scale)^2))*math.sin((-(theta3 + 1.5708)+math.atan((p2.y*scale)/(p2.x*scale))))
+		local shiftBottomPinx = (r4*math.cos(3.14159265358979 - theta4) - r4)*scale
+		local shiftBottomPiny = (r4*math.sin(3.14159265358979 - theta4) - 0.75)*scale
 		
+		local leverAngle = -(THETA2 - 6.085789747)
+		local shiftLeverx = (p1.x - (math.sqrt(p1.x^2 + p1.y^2)*math.cos(leverAngle + math.atan(p1.y/p1.x))))*scale
+		local shiftLevery = -((math.sqrt(p1.x^2 + p1.y^2)*math.sin(leverAngle + math.atan(p1.y/p1.x))) - p1.y)*scale
 		
-		--animate
-		if THETA2 > 7.2 then
-			THETA2 = 5.9
+		--Piecewise solution for the discontinuity in theta3
+		local linkAngle
+		if theta3 > 0 then
+			linkAngle = -(theta3 + 1.5708) + 3.14159
 		else
-			THETA2 = THETA2 + 0.0075
+			linkAngle = -(theta3 + 1.5708)
 		end
-		Actions.waitSeconds(0.001)
+		
+		local shiftLinkx = (math.sqrt((p2.x*scale)^2+(p2.y*scale)^2))*math.cos(math.atan((p2.y*scale)/(p2.x*scale))) - (math.sqrt((p2.x*scale)^2+(p2.y*scale)^2))*math.cos((linkAngle+math.atan((p2.y*scale)/(p2.x*scale))))
+		local shiftLinky = (math.sqrt((p2.x*scale)^2+(p2.y*scale)^2))*math.sin(math.atan((p2.y*scale)/(p2.x*scale))) - (math.sqrt((p2.x*scale)^2+(p2.y*scale)^2))*math.sin((linkAngle+math.atan((p2.y*scale)/(p2.x*scale))))
+		
+		local pusherAngle = 3.14159265358979 - (theta4 + 0.04914073)
+		local shiftPusherx = (p4.x - (math.sqrt(p4.x^2 + p4.y^2))*math.cos(pusherAngle + (math.atan(p4.y/p4.x))))*scale
+		local shiftPushery = (-(math.sqrt(p4.x^2 + p4.y^2))*math.sin(pusherAngle + (math.atan(p4.y/p4.x))) + p4.y)*scale
+		
+--------------------------------------------------------------------------------------------------------------------------------------------------
+		--animate
+		
+		if upButton.pressed then
+			if THETA2 < 7.4 then
+				THETA2 = THETA2 + 0.025
+				Actions.waitSeconds(0.01)
+			else
+				Actions.waitSeconds(0.01)
+			end
+		end
+		if downButton.pressed then
+			if THETA2 > 5.87 then
+				THETA2 = THETA2 - 0.025
+				Actions.waitSeconds(0.01)
+			else
+				Actions.waitSeconds(0.01)
+			end
+		end
+		
+--------------------------------------------------------------------------------------------------------------------------------------------------
+		xform[4]:setPosition(osg.Vec3d(shiftTopPinx, shiftTopPiny, 0))  --top pin good
+		xform[5]:setPosition(osg.Vec3d(shiftBottomPinx, shiftBottomPiny, 0))  --bot pin good
+		--Links
+		xform[8]:setAttitude(osg.Quat(linkAngle,osg.Vec3d(0,0,1)))
+		xform[9]:setAttitude(osg.Quat(linkAngle,osg.Vec3d(0,0,1)))
+		xform[8]:setPosition(osg.Vec3d(shiftTopPinx + shiftLinkx, shiftTopPiny + shiftLinky, 0))
+		xform[9]:setPosition(osg.Vec3d(shiftTopPinx + shiftLinkx, shiftTopPiny + shiftLinky, 0))
+		--Lever
+		xform[10]:setAttitude(osg.Quat(leverAngle,osg.Vec3d(0,0,1)))  --lever good
+		xform[10]:setPosition(osg.Vec3d(shiftLeverx, shiftLevery, 0))
+		--Pusher
+		xform[11]:setAttitude(osg.Quat(pusherAngle,osg.Vec3d(0,0,1)))  --pusher bad
+		xform[11]:setPosition(osg.Vec3d(shiftPusherx, shiftPushery,0))
+		
 		Actions.waitForRedraw()
 	end
 end)
 
-
-
---------------------------------------------------------------------------------------------------------------------------------------------
-
-Actions.addFrameAction(function()
-		while true do
-			
-			xform[4]:setPosition(osg.Vec3d(shift2x, shift2y, 0))  --top bolt good
-			
-			xform[5]:setPosition(osg.Vec3d(shift3x, shift3y, 0))  --bot bolt good
-			
-			xform[8]:setAttitude(osg.Quat(-(theta3 + 1.5708),osg.Vec3d(0,0,1)))
-			xform[9]:setAttitude(osg.Quat(-(theta3 + 1.5708),osg.Vec3d(0,0,1)))
-
-			xform[8]:setPosition(osg.Vec3d(shift2x + shiftLinkx, shift2y + shiftLinky, 0))
-			xform[9]:setPosition(osg.Vec3d(shift2x + shiftLinkx, shift2y + shiftLinky, 0))
-			
-			
-			xform[10]:setAttitude(osg.Quat(dT2,osg.Vec3d(0,0,1)))  --lever good
-			xform[10]:setPosition(osg.Vec3d((p1.x - (math.sqrt(p1.x^2 + p1.y^2)*math.cos(dT2 + math.atan(p1.y/p1.x))))*scale, -((math.sqrt(p1.x^2 + p1.y^2)*math.sin(dT2 + math.atan(p1.y/p1.x))) - p1.y)*scale, 0))
-
-			xform[11]:setAttitude(osg.Quat(3.14159265358979 - theta4,osg.Vec3d(0,0,1)))  --pusher
-			xform[11]:setPosition(osg.Vec3d(     (p4.x - (math.sqrt(p4.x^2 + p4.y^2))*math.cos((3.14159265358979 - theta4) + (math.atan(p4.y/p4.x))))*scale, ((math.sqrt(p4.x^2 + p4.y^2))*math.sin((3.14159265358979 - theta4) + (math.atan(p4.y/p4.x))) - p4.y)*scale,0))
-			
-			Actions.waitForRedraw()
-		end
-	end)
-	
---------------------------------------------------------------------------------------------------------------------------------------------	
-
-	
 --18 = the plunger
 
---todo: add an action to move bolts to the positins p2 and p3, then look into moving the lever, links, and pusher wilhe rotating them
+--todo: add an action to move pins to the positins p2 and p3, then look into moving the lever, links, and pusher wilhe rotating them
